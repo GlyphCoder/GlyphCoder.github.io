1be3ddc60f54df33bc9502915583a9493e8f4a9e
Bhavya Shah <bhavyashah20832083@gmail.com>
2025-09-17 09:34:25 +0530

    added bootstrap

--- bhavyaresumejs.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const spotlight = document.querySelector('.spotlight');
-    spotlight.style.left = e.clientX + 'px';
-    spotlight.style.top = e.clientY + 'px';
-});
